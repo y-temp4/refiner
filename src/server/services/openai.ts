fdaeed522735a9cf1403c24ec31e7d1f98f5d1d6
y-temp4 <y.temp4@gmail.com>
2023-03-27 15:16:30 +0900

    Refine prompt

--- src/server/services/openai.ts
+++ src/server/services/openai.ts
@@ -1,4 +1,8 @@
-import { Configuration, OpenAIApi } from 'openai'
+import {
+  ChatCompletionRequestMessageRoleEnum,
+  Configuration,
+  OpenAIApi,
+} from 'openai'
 
 import { env } from '~/env.mjs'
 
@@ -7,25 +11,36 @@ export const openai = new OpenAIApi(configuration)
 
 export async function generateImprovedCode(code: string) {
   const CHAT_GPT_SYSTEM_PROMPT = `Refactor the code passed to you.
-Only return code, following format.
-\`\`\`\${extension}
-{code}
-\`\`\`\
-`
+Your response should be in JSON format with two parameters 'langCode' and 'code'`
   const completion = await openai.createChatCompletion({
     model: 'gpt-3.5-turbo',
     messages: [
-      { role: 'system', content: CHAT_GPT_SYSTEM_PROMPT },
-      { role: 'user', content: code },
+      {
+        role: ChatCompletionRequestMessageRoleEnum.System,
+        content: CHAT_GPT_SYSTEM_PROMPT,
+      },
+      { role: ChatCompletionRequestMessageRoleEnum.User, content: code },
     ],
     temperature: 0.6,
   })
-  const content = completion.data.choices[0]?.message?.content ?? ''
-  const generatedCode = content.match(/```.*?\n([\s\S]*?)\n```/)?.[1] ?? content
-  const langCode =
-    content
-      .match(/```.*?\n([\s\S]*?)\n```/)?.[0]
-      .split('\n')[0]
-      ?.split('```')[1] ?? ''
-  return { generatedCode, langCode }
+  function parseJSON<T>(text: string): T | { error: string } {
+    try {
+      const response: T = JSON.parse(text)
+      return response
+    } catch {
+      return {
+        error: text ?? 'Unknown error.',
+      }
+    }
+  }
+  const response = parseJSON<{ langCode: string; code: string }>(
+    completion.data.choices[0]?.message?.content ?? ''
+  )
+  if ('error' in response) {
+    return {
+      langCode: 'txt',
+      generatedCode: response.error,
+    }
+  }
+  return { generatedCode: response.code, langCode: response.langCode }
 }
